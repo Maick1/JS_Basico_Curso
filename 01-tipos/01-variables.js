10f7e154402e136977844a701d06b365a11fa124
MayTomaláZ <86171053+Maick1@users.noreply.github.com>
2024-05-26 17:27:59 -0500

    prueba

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,13 @@
+let nombre = "Hola Mundo";
+let NombreCompleto;
+
+//mas se usa el camelCase
+let nombreCompleto;
+
+let nombre_completo;
+
+console.log(nombre);
+
+let apellido;
+
+apellido = 'Tomalá';
